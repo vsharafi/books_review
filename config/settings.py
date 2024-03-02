5cfddcde086b8e09f72ebb0c9033633c389ef273
vahid <vahid.sh1431@yahoo.com>
2024-03-02 23:04:59 +0330

    implement password reset and change

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-
+# crispy forms settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
